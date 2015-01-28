f0f9d36a2054c91ea0df65f889d7a38a68b869dc
Deepa <dee.baliga@gmail.com>
2015-01-27 21:46:28 -0800

    Word Break

---
+++ Dynamic Programming/src/WordBreak.java
@@ -0,0 +1,56 @@
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class WordBreak {
+	
+	
+	public static void main(String[] args) {
+		
+		String s = "leetercode";
+		
+		Set<String> dict = new HashSet<String>();
+		dict.add("leet"); dict.add("code");
+		
+		System.out.println("String can be split " + wordBreak(s, dict));
+		
+	}
+	
+	
+	//Complexity O(size of string * size of dictionary)
+	//t[0] = true initial condition
+	//only when whole word matches , we set true at that index
+	//       L    E     E     T     C    O     D     E
+	//t[0] t[1]  t[2]  t[3]  t[4] t[5]  t[6]  t[7]  t[8]
+	//true false false false true false false false true
+	
+	public static boolean wordBreak(String s, Set<String> dict) {
+        boolean[] t = new boolean[s.length()+1];
+        t[0] = true; 
+ 
+        for(int i=0; i<s.length(); i++){
+            //should continue from match position
+            if(!t[i]) 
+                continue;
+ 
+            for(String a: dict){
+                int len = a.length();
+                int end = i + len;
+                if(end > s.length())
+                    continue;
+ 
+                if(t[end]) continue;
+ 
+                if(s.substring(i, end).equals(a)){
+                    t[end] = true;
+                }
+            }
+        }
+ 
+        for(int i=0; i< s.length()+1 ; i++){
+        	System.out.println("t[" +i+"]" +t[i]);
+        }
+        
+        return t[s.length()];
+    }
+}
