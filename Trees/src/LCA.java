b6a5136163c270a525800b39bce4f54bd5da1b35
Deepa <dee.baliga@gmail.com>
2015-02-03 21:46:11 -0800

    Lowest common ancestor of binary tree

--- Trees/src/LCA.java
+++ Trees/src/LCA.java
@@ -1,4 +1,34 @@
 
 public class LCA {
+	
+	
+	//Complexity O(n)
+	//Pre-order
+	public TreeNode LCA(TreeNode root, TreeNode node1, TreeNode node2){
+		
+		if(root==null){
+			return null;
+		}
+		
+		//check if root is LCA
+		if(root.value == node1.value || root.value == node2.value){
+			return root;
+		}
+		
+		//check LCA in left subtree
+		TreeNode left = LCA(root.left,node1,node2);
+				
+		//check LCA in right subtree
+		TreeNode right = LCA(root.right, node1, node2);
+		
+		
+		//if both subtrees have LCA, then root
+		if(left!=null && right!=null){
+			return root;
+		}
+		
+		return left !=null ? left : right;
+		
+	}
 
 }
