ce8fffbf255cb89927a38c29ca06266aed621bd3
Deepa <dee.baliga@gmail.com>
2015-02-08 19:51:20 -0800

    Deserialize a binary tree

--- Trees/src/Deserialization.java
+++ Trees/src/Deserialization.java
@@ -1,4 +1,34 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
+
+/**
+ * 
+ *Complexity O(n)
+ *
+ */
 public class Deserialization {
-
+	
+	public static void main(String[] args) throws IOException {
+		File file = new File("example.txt");
+		FileReader reader = new FileReader(file);
+	    final BufferedReader input = new BufferedReader(reader);
+	    deserialize(null, input);
+	}
+   
+	
+	public static void deserialize(TreeNode root, final BufferedReader reader) throws IOException {
+		
+		int token = reader.read();
+		
+		if(token == '#'){
+			return ;
+		} else {
+			root = new TreeNode(token);
+			deserialize(root.left, reader);
+			deserialize(root.right, reader);
+		}	
+	}
 }
