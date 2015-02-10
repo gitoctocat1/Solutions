049d804a4ac61b762824e0208ae390ce304a156e
Deepa <dee.baliga@gmail.com>
2015-02-10 00:16:23 -0800

    Search for element in sorted rotated array

--- Arrays/src/SearchRotatedSortedArray.java
+++ Arrays/src/SearchRotatedSortedArray.java
@@ -7,6 +7,13 @@ public class SearchRotatedSortedArray {
 		System.out.println(search(A, 1));
 		
 	}
+	
+	/**
+	 * Complexity O(lg n )
+	 * @param A
+	 * @param target
+	 * @return
+	 */
 	  public static int search(int[] A, int target) {
 	        
 	        if(A.length == 0){
@@ -23,10 +30,12 @@ public class SearchRotatedSortedArray {
 	        	
 	        	int mid = right + (left -right)/2 ;
 	           
+	        	//if target is middle element
 	           if(A[mid] == target){
 	               return mid;
 	           }
 	           
+	           //if target is in lower half of the array
 	           if(A[left]<= A[mid]){
 	               
 	               if(A[left]<= target && target < A[mid]){
@@ -36,6 +45,7 @@ public class SearchRotatedSortedArray {
 	                   
 	               }
 	           } else
+	        	  //if target is in upper half of array 
 	              if(A[right] >=target && target >A[mid]){
 	                  left = mid + 1;
 	              }  else {
