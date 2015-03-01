71239454f341677600f7e8593a8760942d820ba4
Deepa <dee.baliga@gmail.com>
2015-02-28 20:57:06 -0800

    Reverse string

---
+++ Strings/src/ReverseString.java
@@ -0,0 +1,31 @@
+
+public class ReverseString {
+
+	/**
+	 * Complexity O (n)
+	 * @param str
+	 * @return
+	 */
+	public String reverse(String str){
+		
+		String result = null;
+		
+		int i = 0 ; int j = str.length()-1;
+		
+		char[] charArray = str.toCharArray();
+		
+		while (i < j){
+			
+			//swap 
+			char temp = charArray[i];
+			charArray[i] = charArray[j];
+			charArray[j] = temp;
+			
+			i++; j --;
+		}
+		result = new String(charArray);
+		
+		return result;
+		
+	}
+}
