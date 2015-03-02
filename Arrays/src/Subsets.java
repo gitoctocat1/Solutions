0c7d22deda6294e1ba7bba279e008f1f6372d7b0
Deepa <dee.baliga@gmail.com>
2015-03-01 21:44:39 -0800

    Subsets

--- Arrays/src/Subsets.java
+++ Arrays/src/Subsets.java
@@ -100,4 +100,38 @@ public class Subsets {
 		return result;
      
  }
+	 
+	 
+	 /**
+	  * Complexity O(m lg m + n lgn )
+	  *  Return 1 if arr2[] is a subset of arr1[] 
+	  *  */
+	 boolean isSubset(int arr1[], int arr2[], int m, int n)
+	 {
+	     int i = 0, j = 0;
+	      
+	     if(m < n)
+	        return false;
+	  
+	     Arrays.sort(arr1);
+	     Arrays.sort(arr2);
+	     
+	     while( i < n && j < m )
+	     {
+	         if( arr1[j] <arr2[i] )
+	             j++;
+	         else if( arr1[j] == arr2[i] )
+	         {
+	             j++;
+	             i++;
+	         }
+	         else if( arr1[j] > arr2[i] )
+	             return false;
+	     }
+	   
+	     if( i < n )
+	         return false;
+	     else
+	         return true;
+	 }
 }
