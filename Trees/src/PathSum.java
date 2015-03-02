c79ca1172623a61a59a1a06c0171b05db338dcc7
Deepa <dee.baliga@gmail.com>
2015-03-01 21:52:35 -0800

    Print paths that add up to sum

--- Trees/src/PathSum.java
+++ Trees/src/PathSum.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class PathSum {
 
@@ -26,4 +29,50 @@ public class PathSum {
 	        
 	        return left || right;
 	    }
+	 
+	 
+	 
+	 /**
+	  * 
+	  * @param root
+	  * @param sum
+	  * @return
+	  */
+	 public List<List<Integer>> pathSum(TreeNode root, int sum) {
+	        
+	        List<List<Integer>> result = new ArrayList<List<Integer>>();
+			 
+			 List<Integer> currentPath = new ArrayList<Integer>();
+			 
+			 getPath(root, sum, currentPath, result); 
+			 
+			 return result;
+	        
+	    }
+	    
+	    public void getPath(TreeNode root, int sum, List<Integer> currentPath, List<List<Integer>> result ) {
+		        
+		        if(root == null){
+		            return;
+		        }
+		        
+		        currentPath.add(root.value);
+		        
+		        //check if leaf node and sum is 0
+		        if(root.left ==null && root.right ==null && (sum - root.value)==0){
+		            
+		            //this is a valid path so add to the result
+		            List<Integer> list = new ArrayList<Integer>(currentPath);    
+		            result.add(list);
+		        }
+		        
+		        //check if left subtree has a path
+		        getPath(root.left, sum - root.value , currentPath, result);
+		        
+		        //check if right subtree has a path
+		        getPath(root.right, sum - root.value, currentPath, result);
+		        
+		        currentPath.remove(currentPath.size()-1);
+		        
+		    }
 }
