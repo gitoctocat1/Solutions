fe9b4de4c33f711b6820920f4c6bce2617bc5f2a
Deepa <dee.baliga@gmail.com>
2015-04-01 21:38:23 -0700

    Maximum depth paranthesis

---
+++ Strings/src/MaxDepthParanthesis.java
@@ -0,0 +1,42 @@
+
+public class MaxDepthParanthesis {
+	
+	/**
+	 * Complexity O(n) and space complexity O(1)
+	 * @param S
+	 * @return
+	 */
+	int maxDepth(String S)
+	{
+	    int current_max = 0; // current count
+	    int max = 0;    // overall maximum count
+	    int n = S.length();
+	 
+	    // Traverse the input string
+	    for (int i = 0; i< n; i++)
+	    {
+	        if (S.charAt(i) == '(')
+	        {
+	            current_max++;
+	 
+	            // update max if required
+	            if (current_max> max)
+	                max = current_max;
+	        }
+	        else if (S.charAt(i) == ')')
+	        {
+	            if (current_max>0)
+	                current_max--;
+	            else
+	                return -1;
+	        }
+	    }
+	 
+	    // finally check for unbalanced string
+	    if (current_max != 0)
+	        return -1;
+	 
+	    return max;
+	}
+
+}
